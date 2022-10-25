1fd4c26441adac9ae199b855b6d64517b473705a
Andrea Bedini <andrea@andreabedini.com>
2022-10-25 19:34:35 +0800

    Split things up

---
+++ cabal-gen-bounds/app/Main.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Map.Strict as Map
+import Distribution.Client.Compat.Prelude
+import qualified Distribution.Client.InstallPlan as InstallPlan
+import Distribution.Client.ProjectOrchestration
+import Distribution.Client.ProjectPlanning
+import Distribution.Client.Types.PackageLocation
+import Distribution.Client.Types.PackageSpecifier
+import Distribution.Compat.Lens
+import Distribution.Package
+  ( packageName,
+    packageVersion,
+  )
+import Distribution.PackageDescription
+import Distribution.PackageDescription.PrettyPrint
+import Distribution.Solver.Types.SourcePackage
+import Distribution.Types.GenericPackageDescription.Lens
+import qualified Distribution.Verbosity as Verbosity
+import Distribution.Version hiding (hasLowerBound)
+import System.FilePath
+import Prelude ()
+
+main :: IO ()
+main = do
+  let verbosity = Verbosity.normal
+      cliConfig = mempty -- ¯\_(ツ)_/¯
+  ProjectBaseContext
+    { distDirLayout,
+      cabalDirLayout,
+      projectConfig,
+      localPackages
+    } <-
+    establishProjectBaseContext verbosity cliConfig OtherCommand
+
+  (_, elaboratedPlan, _, _, _) <-
+    rebuildInstallPlan
+      verbosity
+      distDirLayout
+      cabalDirLayout
+      projectConfig
+      localPackages
+
+  let versionsMap =
+        Map.fromList $
+          [ (packageName ipi, packageVersion ipi)
+            | InstallPlan.PreExisting ipi <- InstallPlan.toList elaboratedPlan
+          ]
+            ++ [ (packageName ecp, packageVersion ecp)
+                 | InstallPlan.Configured ecp <- InstallPlan.toList elaboratedPlan,
+                   not $ elabLocalToProject ecp
+               ]
+
+  let improveDepedency dep@(Dependency pn vr nes) =
+        case Map.lookup pn versionsMap of
+          Nothing -> dep
+          Just v -> Dependency pn (improveVersionRange vr v) nes
+
+  for_ localPackages $ \case
+    SpecificSourcePackage SourcePackage {srcpkgPackageId, srcpkgSource = LocalUnpackedPackage pkgPath, srcpkgDescription, srcpkgDescrOverride = Nothing} -> do
+      let PackageIdentifier {pkgName} = srcpkgPackageId
+
+      let fp = pkgPath </> unPackageName pkgName <.> "cabal.revised"
+      putStrLn $ "Writing " ++ fp ++ " for " ++ prettyShow srcpkgPackageId
+
+      writeGenericPackageDescription fp $
+        srcpkgDescription & (\f -> allCondTrees $ traverseCondTreeC f) %~ map improveDepedency
+    anyOtherCase ->
+      putStrLn $ "Not handled" ++ show anyOtherCase
+
+improveVersionRange :: VersionRange -> Version -> VersionRange
+improveVersionRange vr v =
+  simplifyVersionRange . addMV v . addUB v . addLB v $ vr
+
+addLB :: Version -> VersionRange -> VersionRange
+addLB v vr
+  | hasLowerBound vr = vr
+  | otherwise = intersectVersionRanges vr (laterVersion v)
+
+addUB :: Version -> VersionRange -> VersionRange
+addUB v vr
+  | hasUpperBound vr = vr
+  | otherwise = intersectVersionRanges vr (earlierVersion $ majorUpperBound v)
+
+addMV :: Version -> VersionRange -> VersionRange
+addMV v vr = unionVersionRanges vr (majorBoundVersion v)
+
+-- The documentation of Distribution.Types.VersioRange.hasLowerBound says
+-- > Note: this function only considers the user-specified lower bounds,
+-- > but not the implicit >=0 lower bound.
+-- but hasLowerBound (orLaterVersion $ mkVersion [0]) = True
+-- This function should give the right answer
+hasLowerBound :: VersionRange -> Bool
+hasLowerBound = not . withinRange (mkVersion [0])
