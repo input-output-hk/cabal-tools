89817a5819782bb3fe800cd31ef57a009ad541d5
Andrea Bedini <andrea@andreabedini.com>
2022-12-23 17:15:51 +0800

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,30 +5,31 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-    in
-      flake-utils.lib.eachSystem supportedSystems (system:
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
       let
-        overlays = [ haskellNix.overlay
+        overlays = [
+          haskellNix.overlay
           (final: prev: {
-            hixProject =
-              final.haskell-nix.hix.project {
-                src = ./.;
-                evalSystem = "x86_64-linux";
-              };
+            hixProject = final.haskell-nix.hix.project {
+              src = ./.;
+              evalSystem = "x86_64-linux";
+            };
           })
         ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hixProject.flake {};
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        flake = pkgs.hixProject.flake { };
       in flake // {
         legacyPackages = pkgs;
-        packages."cabal-gen-bounds" = flake.packages."cabal-tools:exe:cabal-gen-bounds";
-        packages."cabal-builder" = flake.packages."cabal-tools:exe:cabal-builder";
+        package = flake.packages // {
+          "cabal-gen-bounds" =
+            flake.packages."cabal-tools:exe:cabal-gen-bounds";
+          "cabal-builder" = flake.packages."cabal-tools:exe:cabal-builder";
+        };
       });
 
   # --- Flake Local Nix Configuration ----------------------------
@@ -36,8 +37,9 @@
     # This sets the flake to use the IOG nix cache.
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
     allow-import-from-derivation = "true";
   };
 }
