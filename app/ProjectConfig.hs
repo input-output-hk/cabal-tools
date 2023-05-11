9832f9407b2f8b6e1d9d56526090fc4005ab9de8
Andrea Bedini <andrea@andreabedini.com>
2023-05-11 17:06:25 +0800

    Better ProjectConfig

--- app/ProjectConfig.hs
+++ app/ProjectConfig.hs
@@ -3,17 +3,15 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Distribution.Client.DistDirLayout (DistDirLayout (distProjectCacheFile))
-import Distribution.Client.IndexUtils (ActiveRepos, TotalIndexState)
 import Distribution.Client.ProjectConfig (ProjectConfig)
-import Distribution.Client.ProjectPlanning (ElaboratedInstallPlan, ElaboratedSharedConfig)
 import Distribution.Client.Types (PackageSpecifier, UnresolvedSourcePackage)
 import Opts (parseOpts)
 import Text.Pretty.Simple (CheckColorTty (NoCheckColorTty), OutputOptions (outputOptionsCompact, outputOptionsCompactParens), defaultOutputOptionsDarkBg, pPrintOpt)
 import WithCacheFile (withCacheFile)
 
-type Key = (ProjectConfig, [PackageSpecifier UnresolvedSourcePackage], [FilePath])
+type Key = (FilePath, FilePath)
 
-type Value = (ElaboratedInstallPlan, ElaboratedInstallPlan, ElaboratedSharedConfig, TotalIndexState, ActiveRepos)
+type Value = (ProjectConfig, [PackageSpecifier UnresolvedSourcePackage])
 
 pPrint' :: Show a => a -> IO ()
 pPrint' = pPrintOpt NoCheckColorTty defaultOutputOptionsDarkBg {outputOptionsCompact = True, outputOptionsCompactParens = True}
@@ -22,13 +20,16 @@ main :: IO ()
 main = do
   (_prjRoot, distDirLayout) <- parseOpts
 
-  withCacheFile @Key @Value (distProjectCacheFile distDirLayout "improved-plan") $ \case
+  withCacheFile @Key @Value (distProjectCacheFile distDirLayout "config") $ \case
     Left err -> print err
     Right (_monitorStateFileSet, k, Left err) -> do
       print k
       print err
-    Right (_monitorStateFileSet, k, Right _v) -> do
-      let (projectConfig, localPackages, _progSearchPath) = k
+    Right (_monitorStateFileSet, k, Right v) -> do
+      let (configPath, projectFile) = k
+      let (projectConfig, localPackages) = v
 
+      pPrint' configPath
+      pPrint' projectFile
       pPrint' projectConfig
       pPrint' localPackages
